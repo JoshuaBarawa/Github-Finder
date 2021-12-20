c90beaedaac0ed634c86bf11c5118e1c3e5bdc61
Joshua.B.M <mwamlandabarawa@gmail.com>
2021-12-20 09:53:27 +0300

    chnaged the ui

--- index.js
+++ index.js
@@ -2,52 +2,71 @@
 
 document.getElementById('btn').addEventListener('click', async function(){
 
+  function Repo(repoName,language,forks,counts,url){
+    this.repoName = repoName;
+    this.language = language;
+    this.forks = forks;
+    this.counts = counts;
+    this.url = url;
+  }
+
     var name = document.getElementById('search-input').value;
+    var repos = [];
 
   fetch("https://api.github.com/users/" + name)
     .then((response) =>response.json())
     .then((data) => {
         document.getElementById('profile').innerHTML =`
 
-      
     <div class="user-profile">
     <img src=${data.avatar_url} alt="" />
+
+    <div id="all-details">
     <div id="details">
-    <p>Member since: ${data.created_at}</p>
-       <p>Name: ${data.login}</p>
-       <p>Location: ${data.location}</p>
-       <p>Repos: ${data.public_repos}</p>
-       <span>Followers: ${data.followers}</span> <span>Following:${data.following}</span>
-      <a href="${data.html_url}" target="_blank" >View Profile</a>
-      </div>
+    <div>
+    <p>${data.name}</p>
+    <a href="${data.html_url}" target="_blank" >@${data.login}</a>
+    </div>
+    <div>
+    <p>Joined ${data.created_at}</p>
+    </div>
+    </div>
+   
+   <div id="follow">
+   <p>Repos ${data.public_repos}</p>
+   <p>Followers ${data.followers}</p>
+   <p>Following ${data.following}</p>
+   </div>
+   <p>Location: ${data.location}</p>
+    </div>
     </div>
         `
-      
       });
 
 
       fetch("https://api.github.com/users/" + name + "/repos")
-    .then((response) =>response.json())
+     
+    .then((response) => response.json())
     .then((data) => {
-
+      document.getElementById('repos').innerHTML ="";
+      
       data.forEach(data => {
-
+        var repo = new Repo(data.name,data.language,data.forks,data.watchers,data.stargazers_count,data.html_url)
+        repos.push(repo);
         document.getElementById('repos').innerHTML +=`
+  
         <div class="user-repo">
-        <p>${data.name}</p>
-           <p>${data.description}</p>
-           <p>Language: ${data.language}</p>
+        <p>${repo.repoName}</p>
+           <p>${repo.language}</p>
            <div id="other">
-           <p>${data.visibility}</p>
-           <p>Forks: ${data.forks}</p>
-           <p>Watchers: ${data.watchers}</p>
-           <p>Stars: ${data.stargazers_count}</p>
+           <p>Forks: ${repo.forks}</p>
+           <p>Stars: ${repo.counts}</p>
            </div>
-          <a href="${data.html_url}" target="_blank" >Source Code</a>
+          <a href="${repo.url}" target="_blank" >Source Code</a>
         </div>`
-      
+       
       })
-      
+
       });
 
 });
